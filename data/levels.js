d2b13e7494e27238334bc77128c80c2b8e692f23
thisisuhhplanetring <vildancecen@gmail.com>
2026-08-19 23:14:31 +0300

    Add files via upload

--- data/levels.js
+++ data/levels.js
@@ -1,6 +1,16 @@
 window.WBDL_LEVELS = {
   "levels": [
     {
+      "name": "5wbdl5",
+      "publisher": "Helloguy0",
+      "points": 250,
+      "verifier": "...its impossible.",
+      "id": "147321104",
+      "version": "2.2",
+      "added": "19 August 2026",
+      "image": "https://www.youtube.com/embed/KNSw4GypZ24"
+    },
+    {
       "name": "WBPILL Level",
       "publisher": "TurkBall",
       "points": 250,
